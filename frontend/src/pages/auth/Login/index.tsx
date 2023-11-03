25bc8f9b9677d9422317f35b8a4f73d42f08c8cf
Nirajan Kunwor <nirajankunwor604@gmail.com>
2023-11-03 17:53:48 +0545

    feat: login setup, redux persist setup

--- frontend/src/pages/auth/Login/index.tsx
+++ frontend/src/pages/auth/Login/index.tsx
@@ -1,5 +1,105 @@
+import Container from "@mui/material/Container";
+import { Col, Form, Row } from "react-bootstrap";
+import TextField from "@mui/material/TextField";
+import Button from "@mui/material/Button";
+import { Formik } from "formik";
+import { object, string } from "yup";
+import { AuthInterface } from "../../../interface/auth.interface";
+import { postData } from "../../../services/axios.service";
+import { useNavigate } from "react-router-dom";
+import { successToast } from "../../../services/toaster.service";
+import { useDispatch } from "react-redux";
+import { login } from "../../../slice/authSlice";
+
 const Login = () => {
-  return <div>Login</div>;
+  let initialValues = {
+    email: "",
+    password: "",
+  };
+
+  const dispatch = useDispatch();
+
+  const navigate = useNavigate();
+
+  let authValidationSchema = object({
+    email: string().email().required("Email is a required field."),
+    password: string()
+      .min(8, "Minimum length of password should be 8")
+      .required("Password is a required field."),
+  });
+
+  const loginHandler = async (values: AuthInterface) => {
+    const resp = await postData("/auth/login", values);
+
+    if (resp.status === "success") {
+      const data = {
+        jwt: resp.token,
+        role: resp.authData.role,
+        email: resp.authData.email,
+      };
+      dispatch(login(data));
+      navigate("/products");
+      successToast("User logged in successfully");
+    }
+  };
+  return (
+    <Container>
+      <Row className="d-flex justify-content-center">
+        <Col xs={12} md={6}>
+          <h1>Sign In</h1>
+          <Formik
+            initialValues={initialValues}
+            validationSchema={authValidationSchema}
+            onSubmit={loginHandler}
+          >
+            {({ handleChange, handleSubmit, errors, touched, handleBlur }) => (
+              <Form onSubmit={handleSubmit}>
+                <div className="mb-4">
+                  <TextField
+                    id="email"
+                    name="email"
+                    variant="outlined"
+                    required
+                    onBlur={handleBlur}
+                    fullWidth
+                    label="Email"
+                    placeholder="Enter email here"
+                    autoFocus
+                    onChange={handleChange}
+                  />
+
+                  <span className="text-danger">
+                    {touched.email && errors.email}
+                  </span>
+                </div>
+
+                <div className="mb-4">
+                  <TextField
+                    id="password"
+                    name="password"
+                    variant="outlined"
+                    required
+                    fullWidth
+                    onBlur={handleBlur}
+                    label="Password"
+                    placeholder="Enter password here"
+                    onChange={handleChange}
+                  />
+                  <span className="text-danger">
+                    {touched.password && errors.password}
+                  </span>
+                </div>
+
+                <Button type="submit" variant="contained">
+                  Login
+                </Button>
+              </Form>
+            )}
+          </Formik>
+        </Col>
+      </Row>
+    </Container>
+  );
 };
 
 export default Login;
--- frontend/src/pages/auth/SignUp/index.tsx
+++ frontend/src/pages/auth/SignUp/index.tsx
@@ -10,6 +10,8 @@ import {
 } from "../../../services/toaster.service";
 import axios from "axios";
 import { useNavigate } from "react-router-dom";
+import { config } from "../../../config";
+import { postData } from "../../../services/axios.service";
 
 const SignUp = () => {
   const [name, setName] = useState("");
@@ -30,79 +32,73 @@ const SignUp = () => {
         password,
         email,
       };
-      try {
-        const response = await axios.post(
-          "http://localhost:8080/api/v1/auth/register",
-          data
-        );
-
-        if (response.data.status) {
-          navigate("/");
-          successToast(response.data.message);
-        }
-      } catch (error: any) {
-        errorToast(error.response.data.error);
+      const response = await postData("/auth/register", data);
+      if (response.status) {
+        navigate("/");
+        successToast(response.message);
       }
     }
   };
   return (
-    <Container>
-      <Row className="d-flex justify-content-center">
-        <Col xs={12} md={6}>
-          <h1>Sign In</h1>
-          <Form onSubmit={registerSubmitHandler}>
-            <TextField
-              id="name"
-              variant="outlined"
-              className="mb-4"
-              required
-              fullWidth
-              label="Name"
-              placeholder="Enter name here"
-              autoFocus
-              onChange={(e) => setName(e.target.value)}
-            />
-            <TextField
-              id="email"
-              variant="outlined"
-              className="mb-4"
-              required
-              fullWidth
-              label="Email"
-              placeholder="Enter email here"
-              autoFocus
-              onChange={(e) => setEmail(e.target.value)}
-            />
-            <TextField
-              id="password"
-              variant="outlined"
-              className="mb-4"
-              required
-              fullWidth
-              label="Password"
-              placeholder="Enter password here"
-              autoFocus
-              onChange={(e) => setPassword(e.target.value)}
-            />
-            <TextField
-              id="confirm-password"
-              variant="outlined"
-              className="mb-4"
-              required
-              fullWidth
-              label="Confirm Password"
-              placeholder="Enter password again"
-              autoFocus
-              onChange={(e) => setConfirmPassword(e.target.value)}
-            />
+    <div className="">
+      <Container>
+        <Row className="d-flex justify-content-center">
+          <Col xs={12} md={6}>
+            <h1>Sign Up</h1>
+            <Form onSubmit={registerSubmitHandler}>
+              <TextField
+                id="name"
+                variant="outlined"
+                className="mb-4"
+                required
+                fullWidth
+                label="Name"
+                placeholder="Enter name here"
+                autoFocus
+                onChange={(e) => setName(e.target.value)}
+              />
+              <TextField
+                id="email"
+                variant="outlined"
+                className="mb-4"
+                required
+                fullWidth
+                label="Email"
+                placeholder="Enter email here"
+                autoFocus
+                onChange={(e) => setEmail(e.target.value)}
+              />
+              <TextField
+                id="password"
+                variant="outlined"
+                className="mb-4"
+                required
+                fullWidth
+                label="Password"
+                placeholder="Enter password here"
+                autoFocus
+                onChange={(e) => setPassword(e.target.value)}
+              />
+              <TextField
+                id="confirm-password"
+                variant="outlined"
+                className="mb-4"
+                required
+                fullWidth
+                label="Confirm Password"
+                placeholder="Enter password again"
+                autoFocus
+                onChange={(e) => setConfirmPassword(e.target.value)}
+              />
 
-            <Button type="submit" variant="contained">
-              Signup
-            </Button>
-          </Form>
-        </Col>
-      </Row>
-    </Container>
+              <Button type="submit" variant="contained">
+                Signup
+              </Button>
+            </Form>
+          </Col>
+        </Row>
+      </Container>
+    </div>
   );
 };
 
